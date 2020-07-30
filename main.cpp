4be056d3b1fe92d72cb9d79b4c0a47c5a4c53458
olgalaskul <lashkul@skytracking.ru>
2020-07-30 17:24:54 +0300

    loof for secind "f" in the string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string str;
+    int res = -2;
+
+    cin >> str;
+
+    for (int i = 0; i <= str.size(); i++) {
+        if (str[i] != 'f')
+            continue;
+
+        res++;
+
+        if (res == 0) {
+            res = i;
+            break;
+        }
+    }
+
+    cout << res << endl;
+
     return 0;
 }
