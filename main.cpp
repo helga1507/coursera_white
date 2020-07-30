bfcf85e44ed2a8d2ffff87006bbb8fbef5e24c84
olgalaskul <lashkul@skytracking.ru>
2020-07-30 17:50:41 +0300

    greatest common divisor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+
+    cin >> a >> b;
+
+    while (a != 0 && b != 0) {
+        if (a > b)
+            a = a % b;
+        else
+            b = b % a;
+    }
+
+    cout << a + b << endl;
+
     return 0;
 }
