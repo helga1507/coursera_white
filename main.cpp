92dd7ad5de45061cec9b2ac6d4876e5a2f09d922
olgalaskul <lashkul@skytracking.ru>
2020-07-30 14:02:37 +0300

    calculation purchase price

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float n, a, b, x, y, price;
+
+    cin >> n >> a >> b >> x >> y;
+
+    if (n > b) {
+        price = n - n * (y / 100);
+    } else if (n > a) {
+        price = n - n * (x / 100);
+    } else {
+        price = n;
+    }
+
+    cout << price << endl;
+
     return 0;
 }
