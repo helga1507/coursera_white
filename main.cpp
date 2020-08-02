070016b5d4e6e27a775dd0084fa4cfc198e5b8a9
olgalaskul <lashkul@skytracking.ru>
2020-08-02 13:31:16 +0300

    factorial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+int Factorial (int a) {
+    if (a < 0)
+        return 1;
+
+    int sum = 1;
+
+    for (int i = 1; i <= a; i++) {
+        sum *= i;
+    }
+
+    return sum;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a;
+
+    cin >> a;
+
+    cout << Factorial(a) << endl;
+
     return 0;
 }
