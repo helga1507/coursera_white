c3704c306fbd7d3966df1d7342cb2992d8898580
olgalaskul <lashkul@skytracking.ru>
2020-08-02 15:16:58 +0300

    move strings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void MoveStrings(vector<string>& source, vector<string>& destination) {
+    for (auto s : source) {
+        destination.push_back(s);
+    }
+
+    source.clear();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> source = {"a", "b", "c"};
+    vector<string> destination = {"z"};
+    MoveStrings(source, destination);
+
+    for (auto d : destination)
+        cout << d << " ";
+
+    cout << endl << "size of source - " << source.size() << endl;
+
     return 0;
 }
