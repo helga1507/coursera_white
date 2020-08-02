d605a7952460d4df94e5cf2d19d936905054f122
olgalaskul <lashkul@skytracking.ru>
2020-08-02 17:00:26 +0300

    average_temperature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getAverage (const vector<int>& vec) {
+    int average = 0;
+
+    for (auto v : vec)
+        average += v;
+
+    return average / vec.size();
+}
+
+vector<int> getVectorBiggerAverage (const vector<int>& temperature) {
+    vector<int> temp_bigger_average;
+
+    int average = getAverage(temperature);
+    int i = 0;
+
+    for (const auto t : temperature) {
+        if (t > average)
+            temp_bigger_average.push_back(i);
+
+        i++;
+    }
+
+    return temp_bigger_average;
+}
+
+vector<int> inputVector (int n) {
+    vector<int> temperature(n);
+
+    for (auto& t : temperature)
+        cin >> t;
+
+    return temperature;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+
+    cin >> n;
+
+    vector<int> temperature = inputVector(n);
+    vector<int> temp_bigger_average = getVectorBiggerAverage(temperature);
+
+    cout << temp_bigger_average.size() << endl;
+
+    for (auto t : temp_bigger_average)
+        cout << t << " ";
+
     return 0;
 }
