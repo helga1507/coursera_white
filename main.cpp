f2b42f13ddb58553085050b6a37fd3a76c833cee
olgalaskul <lashkul@skytracking.ru>
2020-08-02 13:43:51 +0300

    palindrom

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool IsPalindrom (string str) {
+    if (str == "")
+        return true;
+
+    string copy_str = str;
+    reverse(copy_str.begin(), copy_str.end());
+
+    return str == copy_str;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string a;
+
+    cin >> a;
+
+    cout << IsPalindrom(a) << endl;
+
     return 0;
 }
