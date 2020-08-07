fb49d5877b9b862afd7026cdc475338b64e7953a
olgalaskul <lashkul@skytracking.ru>
2020-08-07 17:40:02 +0300

    multiplicity dictionary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+set<string> BuildMapValuesSet (const map<int, string>& m) {
+    set<string> s;
+
+    for (const auto& [key, value] : m)
+        s.insert(value);
+
+    return s;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    set<string> values = BuildMapValuesSet({
+           {1, "odd"},
+           {2, "even"},
+           {3, "odd"},
+           {4, "even"},
+           {5, "odd"}
+    });
+
+    for (const string& value : values) {
+        cout << value << endl;
+    }
+
     return 0;
 }
