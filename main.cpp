7a0843111a6e011545979fb7f103649a410cfab4
olgalaskul <lashkul@skytracking.ru>
2020-08-09 20:10:57 +0300

    sort numbers by module

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+
+    cin >> n;
+
+    vector<int> numbers(n);
+
+    for (auto& num : numbers)
+        cin >> num;
+
+    sort(numbers.begin(), numbers.end(), [] (int& a, int& b) {return abs(a) < abs(b);});
+
+    for (const auto& num : numbers)
+        cout << num << " ";
+
     return 0;
 }
