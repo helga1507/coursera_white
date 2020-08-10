8d54046061a63979ffc58b523ed142e28d2fcdda
olgalaskul <lashkul@skytracking.ru>
2020-08-10 16:33:44 +0300

    sort without register

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+char ToLower (char x) {
+    return tolower(x);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+
+    cin >> n;
+
+    vector<string> strs(n);
+
+    for (auto& s: strs)
+        cin >> s;
+
+    sort(strs.begin(), strs.end(), [] (string a, string b) {
+        transform(a.begin(), a.end(), a.begin(), ToLower);
+        transform(b.begin(), b.end(), b.begin(), ToLower);
+
+        return a < b;
+    });
+
+    for (const auto& s: strs)
+        cout << s << " ";
+
     return 0;
 }
