d2ab2660fce868a5112f4650fe19a6f99f53fd76
olgalaskul <lashkul@skytracking.ru>
2020-08-14 19:16:06 +0300

    Reversible String

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class ReversibleString {
+public:
+    ReversibleString() {}
+
+    ReversibleString(string str) {
+        this->main_str = str;
+    }
+
+    string ToString() const {
+        return this->main_str;
+    }
+
+    void Reverse () {
+        reverse(this->main_str.begin(), this->main_str.end());
+    }
+
+private:
+    string main_str;
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ReversibleString s("live");
+    s.Reverse();
+    cout << s.ToString() << endl;
+
+    s.Reverse();
+    const ReversibleString& s_ref = s;
+    string tmp = s_ref.ToString();
+    cout << tmp << endl;
+
+    ReversibleString empty;
+    cout << '"' << empty.ToString() << '"' << endl;
+
     return 0;
 }
