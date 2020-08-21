7af56b150a6a7cfdcacece824cf2f0bda2567a1d
olgalaskul <lashkul@skytracking.ru>
2020-08-21 17:49:51 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fstream input("../input.txt");
+
+    if (input) {
+        double num;
+
+        cout << fixed << setprecision(3);
+
+        while (input >> num) {
+            cout << num << endl;
+        }
+    }
+
     return 0;
 }
