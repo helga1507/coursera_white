d471ca2df78c69c0e2b9fdd53275b0e0b1c23ad8
olgalaskul <lashkul@skytracking.ru>
2020-08-21 17:37:05 +0300

    read and write files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream input("../input.txt");
+    ofstream output("../output.txt");
+
+    if (input.is_open()) {
+        string line;
+
+        while (getline(input, line)) {
+            output << line << endl;
+        }
+    }
+
     return 0;
 }
