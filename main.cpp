576411c312132838e8774cf0525aac01980bbfe9
olgalaskul <lashkul@skytracking.ru>
2020-08-26 17:36:46 +0300

    class rational

--- main.cpp
+++ main.cpp
@@ -1,6 +1,196 @@
 #include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int GreatestCommonDivisor (int a, int b) {
+    while (a != 0 && b != 0) {
+        if (a > b)
+            a = a % b;
+        else
+            b = b % a;
+    }
+
+    return a + b;
+}
+
+class Rational {
+public:
+    Rational() {
+        this->numerator = 0;
+        this->denominator = 1;
+    };
+
+    Rational(int num, int denom) {
+        this->numerator = num;
+        this->denominator = denom;
+
+        int gcd_num = GreatestCommonDivisor(abs(this->numerator), abs(this->denominator));
+
+        this->numerator = this->numerator / gcd_num;
+        this->denominator = this->denominator / gcd_num;
+
+        if (this->denominator < 0) {
+            this->numerator *= -1;
+            this->denominator *= -1;
+        }
+
+        if (this->numerator == 0)
+            this->denominator = 1;
+    };
+
+    int Numerator() const {
+        return this->numerator;
+    };
+    int Denominator() const {
+        return this->denominator;
+    };
+
+private:
+    int numerator, denominator;
+};
+
+Rational operator+ (const Rational& lhs, const Rational& rhs) {
+    return {lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator()};
+}
+
+Rational operator- (const Rational& lhs, const Rational& rhs) {
+    return {lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator()};
+}
+
+Rational operator* (const Rational& lhs, const Rational& rhs) {
+    return {lhs.Numerator() * rhs.Numerator(), lhs.Denominator() * rhs.Denominator()};
+}
+
+Rational operator/ (const Rational& lhs, const Rational& rhs) {
+    return {lhs.Numerator() * rhs.Denominator(), lhs.Denominator() * rhs.Numerator()};
+}
+
+bool operator== (const Rational& lhs, const Rational& rhs) {
+    return lhs.Numerator() == rhs.Numerator() && lhs.Denominator() == rhs.Denominator();
+}
+
+bool operator< (const Rational& lhs, const Rational& rhs) {
+    return (lhs - rhs).Numerator() < 0;;
+}
+
+ostream& operator<< (ostream& stream, const Rational& num) {
+    stream << num.Numerator() << "/" << num.Denominator();
+
+    return stream;
+}
+
+istream& operator>> (istream& stream, Rational& num) {
+    int a, b;
+    stream >> a;
+    stream.ignore(numeric_limits<streamsize>::max(), '/');
+    stream >> b;
+
+    if (stream)
+        num = Rational(a, b);
+
+    return stream;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    {
+        ostringstream output;
+        output << Rational(-6, 8);
+        if (output.str() != "-3/4") {
+            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
+            return 1;
+        }
+    }
+
+    {
+        istringstream input("5/7");
+        Rational r;
+        input >> r;
+        bool equal = r == Rational(5, 7);
+        if (!equal) {
+            cout << "5/7 is incorrectly read as " << r << endl;
+            return 2;
+        }
+    }
+
+    {
+        istringstream input("");
+        Rational r;
+        bool correct = !(input >> r);
+        if (!correct) {
+            cout << "Read from empty stream works incorrectly" << endl;
+            return 3;
+        }
+    }
+
+    {
+        istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+            return 4;
+        }
+
+        input >> r1;
+        input >> r2;
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+            return 5;
+        }
+    }
+
+    {
+        istringstream input1("1*2"), input2("1/"), input3("/4");
+        Rational r1, r2, r3;
+        input1 >> r1;
+        input2 >> r2;
+        input3 >> r3;
+        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
+        if (!correct) {
+            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
+                 << r1 << " " << r2 << " " << r3 << endl;
+
+            return 6;
+        }
+    }
+    {
+        const set<Rational> rs = {{1, 2}, {1, 25}, {3, 4}, {3, 4}, {1, 2}};
+        if (rs.size() != 3) {
+            cout << "Wrong amount of items in the set" << endl;
+            return 1;
+        }
+
+        vector<Rational> v;
+        for (auto x : rs) {
+            v.push_back(x);
+        }
+        if (v != vector<Rational>{{1, 25}, {1, 2}, {3, 4}}) {
+            cout << "Rationals comparison works incorrectly" << endl;
+            return 2;
+        }
+    }
+
+    {
+        map<Rational, int> count;
+        ++count[{1, 2}];
+        ++count[{1, 2}];
+
+        ++count[{2, 3}];
+
+        if (count.size() != 2) {
+            cout << "Wrong amount of items in the map" << endl;
+            return 3;
+        }
+    }
+
+    cout << "OK" << endl;
     return 0;
 }
