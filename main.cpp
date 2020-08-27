7b564851b9b8a0e900f81814f125680dcdca1036
olgalaskul <lashkul@skytracking.ru>
2020-08-27 16:10:26 +0300

    exception server error

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <system_error>
+
+using namespace std;
+
+string AskTimeServer() {
+    throw system_error(error_code());
+    throw exception();
+    return "00:20:45";
+}
+
+class TimeServer {
+public:
+    string GetCurrentTime() {
+        try {
+            this->last_fetched_time = AskTimeServer();
+        } catch (system_error& e) {}
+
+        return this->last_fetched_time;
+    };
+private:
+    string last_fetched_time = "00:00:00";
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TimeServer ts;
+    try {
+        cout << ts.GetCurrentTime() << endl;
+    } catch (exception& e) {
+        cout << "Exception got: " << e.what() << endl;
+    }
     return 0;
 }
